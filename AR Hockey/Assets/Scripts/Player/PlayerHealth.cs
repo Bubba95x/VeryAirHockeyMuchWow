8ef663e18def61ab3081a3398d2a7c2403af7cd6
Elijah Gjerdingen <elijah.gjerdingen@ndus.edu>
2018-05-08 16:48:41 -0500

    changes to scripts

---
+++ AR Hockey/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+
+	Animator anim;
+	// Use this for initialization
+	void Awake () {
+		currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	public void TakeDamage (int amount) {
+		currentHealth -= amount;
+		healthSlider.value = currentHealth;
+		if (currentHealth <= 0) 
+		{
+			Destroy (this);
+		}
+	}
+}
